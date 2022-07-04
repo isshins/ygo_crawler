06929284c913b1df38d17dcb406b518f9f7abbc8
Isshin <one112heart@gmail.com>
2022-07-04 17:51:10 +0900

    card_crawlerの共通箇所をbase_card_crawlerにまとめた

--- lib/card_crawler/card_master_crawler.rb
+++ lib/card_crawler/card_master_crawler.rb
@@ -1,9 +1,6 @@
-require 'nokogiri'
-require 'open-uri'
+require_relative 'base_card_crawler'
 
-class CardMasterCrawler
-  BASE_URL = 'https://www.db.yugioh-card.com/'
-
+class CardMasterCrawler < BaseCardCrawler
   def crawl(url)
     html = `#{curl_request(url)}`
     doc = Nokogiri::HTML.parse(html)
@@ -69,27 +66,6 @@ class CardMasterCrawler
       end
     end
     type_info
-  end
-
-  def curl_request(url)
-    <<~CURL
-  curl "#{url}" \
-  -H 'authority: www.db.yugioh-card.com' \
-  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
-  -H 'accept-language: ja,en-US;q=0.9,en;q=0.8' \
-  -H 'cache-control: max-age=0' \
-  -H 'referer: https://www.db.yugioh-card.com/yugiohdb/' \
-  -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "macOS"' \
-  -H 'sec-fetch-dest: document' \
-  -H 'sec-fetch-mode: navigate' \
-  -H 'sec-fetch-site: same-origin' \
-  -H 'sec-fetch-user: ?1' \
-  -H 'upgrade-insecure-requests: 1' \
-  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Safari/537.36' \
-  --compressed
-    CURL
   end
 end
 
