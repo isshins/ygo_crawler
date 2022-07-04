e88bc0513f715e1b2f1ca2da185d80aa1ec7eceb
Isshin <one112heart@gmail.com>
2022-07-05 01:17:44 +0900

    レアリティや封入パック情報をクロールするcards_crawler作成

--- lib/card_crawler/source_crawler.rb
+++ lib/card_crawler/source_crawler.rb
@@ -17,7 +17,9 @@ class SourceCrawler < BaseCardCrawler
           source_hash[:title] = pack.at_css('strong').text.strip
           source_hash[:sub_title] = pack.at_css('p').text.gsub(source_hash[:title], '').gsub(/[[:space:]]/,'')
           list_url_href = pack.at_css('.link_value')[:value]
-          source_hash[:list_url] = URI.join(BASE_URL, list_url_href).to_s
+          list_uri = URI.join(BASE_URL, list_url_href)
+          source_hash[:list_url] = list_uri.to_s
+          source_hash[:pack_id] = Hash[URI.decode_www_form(list_uri.query)][:pid]
           source_hash_list << source_hash.dup
         end
       end
